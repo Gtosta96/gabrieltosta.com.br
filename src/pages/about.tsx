c1fc79766fa93b59e8a437761c45915244c30c3b
Guilherme Tosta <guilherme_tosta@hotmail.com>
2021-11-03 21:29:55 -0300

    Adicionado estilização nas paginas, responsividade, imagens, ajustado bug da pagina do youtube, etc...

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Image from 'next/image';
+
 import { Content } from '../components/Content/Content';
 import { Main } from '../layout/Main';
 import { Meta } from '../layout/Main/Meta';
@@ -15,12 +17,23 @@ const About = () => (
     )}
   >
     <Content>
-      <p>
-        Olá, meu nome é Gabriel Tosta, sou desenvolvedor autodidata apaixonado por javascript.
-        Trabalho desde 2015 principalmente com front-end, mas também com uma baita experiência em
-        back-end e infraestrutura.
+      <div className="w-full text-center">
+        <Image
+          className="rounded-full"
+          src="https://media-exp1.licdn.com/dms/image/C4D03AQEa1JupM6oKow/profile-displayphoto-shrink_800_800/0/1581986697399?e=1640822400&v=beta&t=ZnToXoSeE27BVvdgSpwj3UIh_2ypnjH3UVGgbT8TbuU"
+          height="200"
+          width="200"
+        />
+      </div>
+      <p className="p-2">
+        Olá, meu nome é Gabriel Tosta , sou desenvolvedor autodidata apaixonado por javascript .
+        <section>
+          <br />
+          Trabalho desde 2015 principalmente com front-end, mas também com uma baita experiência em
+          back-end e infraestrutura.
+        </section>
       </p>
-      <p>
+      <p className="p-2">
         Ajudo a organizar comunidades locais de tecnologia na cidade de Sorocaba - SP e também sou
         palestrante, onde já tive a oportunidade de compartilhar conhecimento em varias conferências
         de tecnologia - Geekle, BrazilJS, TDC, etc... - com o objetivo de espalhar para o mundo o
